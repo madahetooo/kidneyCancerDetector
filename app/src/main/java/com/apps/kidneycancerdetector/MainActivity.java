3d89261208db7201e03d2467478b6f7d29c0aabf
madahetooo <ieslammedhat@gmail.com>
2021-08-30 13:43:28 +0200

    Adding project

---
+++ app/src/main/java/com/apps/kidneycancerdetector/MainActivity.java
@@ -0,0 +1,128 @@
+package com.apps.kidneycancerdetector;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.common.FirebaseMLException;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.automl.FirebaseAutoMLLocalModel;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
+import com.google.firebase.ml.vision.label.FirebaseVisionOnDeviceAutoMLImageLabelerOptions;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseVisionImageLabeler labeler; //For running the image labeler
+    FirebaseVisionOnDeviceAutoMLImageLabelerOptions.Builder optionsBuilder; // Which option is use to run the labeler local or remotely
+    ProgressDialog progressDialog; //Show the progress dialog while model is downloading...
+    FirebaseModelDownloadConditions conditions; //Conditions to download the model
+    FirebaseVisionImage image; // preparing the input image
+    TextView textView; // Displaying the label for the input image
+    Button button; // To select the image from device
+    ImageView imageView; //To display the selected image
+    Button intent ;  // this is button intent between pages
+    private FirebaseAutoMLLocalModel localModel;
+
+    //Creating view
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text);
+        button = findViewById(R.id.selectImage);
+        imageView = findViewById(R.id.image);
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Please Wait...");
+        progressDialog.setCanceledOnTouchOutside(false);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CropImage.activity().start(MainActivity.this);
+                //fromRemoteModel();
+            }
+        });
+    }
+    //Passing local model as object
+    private void setLabelerFromLocalModel(Uri uri) {
+        localModel = new FirebaseAutoMLLocalModel.Builder()
+                .setAssetFilePath("model/manifest.json")
+                .build();
+        try {
+            FirebaseVisionOnDeviceAutoMLImageLabelerOptions options =
+                    new FirebaseVisionOnDeviceAutoMLImageLabelerOptions.Builder(localModel)
+                            .setConfidenceThreshold(0.0f)
+                            .build();
+            labeler = FirebaseVision.getInstance().getOnDeviceAutoMLImageLabeler(options);
+            image = FirebaseVisionImage.fromFilePath(MainActivity.this, uri);
+            processImageLabeler(labeler, image);
+        } catch (FirebaseMLException | IOException e) {
+            // ...
+        }
+    }
+
+
+    //Open Gallery
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                if (result != null) {
+                    Uri uri = result.getUri(); //path of image in phone
+                    imageView.setImageURI(uri); //set image in imageview
+                    textView.setText(""); //so that previous text don't get append with new one
+                    setLabelerFromLocalModel(uri);
+                   // setLabelerFromRemoteLabel(uri);
+                    LinearLayout linearLayout = findViewById(R.id.linearlayout);
+                    linearLayout.setVisibility(View.VISIBLE);
+                } else
+                    progressDialog.cancel();
+            } else
+                progressDialog.cancel();
+        }
+    }
+
+    //Process image
+    private void processImageLabeler(FirebaseVisionImageLabeler labeler, FirebaseVisionImage image) {
+        labeler.processImage(image).addOnCompleteListener(new OnCompleteListener<List<FirebaseVisionImageLabel>>() {
+            @Override
+            public void onComplete(@NonNull Task<List<FirebaseVisionImageLabel>> task) {
+                progressDialog.cancel();
+                for (FirebaseVisionImageLabel label : task.getResult()) {
+                    String eachlabel = label.getText().toUpperCase();
+                    float confidence = label.getConfidence();
+                    textView.append(eachlabel + " - " + ("" + confidence * 100).subSequence(0, 4) + "%" + "\n\n");
+                }
+//
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("OnFail", "" + e);
+                Toast.makeText(MainActivity.this, "Something went wrong! " + e, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
